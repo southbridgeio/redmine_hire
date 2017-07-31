4fc542366811c422ff81453776c04831f8a1f242
Merff <merff.555@gmail.com>
2017-07-31 21:37:01 +0700

    making hh_api service

--- app/services/hh_api.rb
+++ app/services/hh_api.rb
@@ -1,11 +1,21 @@
+require 'net/http'
+require "uri"
+
 class HhApi
+
+  ACCESS_TOKEN = 'OH8I8E971RUO873RODIF7K62RPQHTPVMRJTJFITB3IH280UCHARB42ALSJV5US6Q'
+  EMPLOYER_ID = '1193714'
+  USER_AGENT = 'Redmine/redmine_hire_plugin'
+  BASE_URL = 'https://api.hh.ru/'
+
 
   def initialize
   end
 
   # GET /employers/{employer_id}/vacancies/active
   def get_active_vacancies_ids
-    # do request, get response
+    byebug
+    response = api_get("#{BASE_URL}employers/#{EMPLOYER_ID}/vacancies/active")
     vacancies_ids = []
     response['items'].each do |item|
       vacancies_ids << item['id']
@@ -16,8 +26,23 @@ class HhApi
   def get_response_collections(vacancies_ids)
     vacancies_ids.each do |vacancy_id|
       # do request, get response
+    end
+  end
 
+  private
 
+  def api_get(url)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    #http.use_ssl = true
+    header = {
+      "Content-Type" => "application/json",
+      "Authorization" => "Bearer #{ACCESS_TOKEN}",
+      "User-Agent" => USER_AGENT
+    }
+    request = Net::HTTP::Get.new(uri.request_uri, header)
+    response = http.request(request)
+    JSON.parse(response.body)
   end
 
 
