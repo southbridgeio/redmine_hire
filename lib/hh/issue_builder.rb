faeb8bf871d6be8c747c6fbbb0bf621d93d3561a
Merff <merff.555@gmail.com>
2017-08-17 21:22:20 +0700

    text editing & column type bug fix

--- lib/hh/issue_builder.rb
+++ lib/hh/issue_builder.rb
@@ -19,30 +19,34 @@ module Hh
         i.status = IssueStatus.find_by(name: ISSUE_STATUS)
         i.tracker = Tracker.find_by(name: ISSUE_TRACKER)
         i.author = User.find_by(login: ISSUE_AUTOR)
+        i.description = build_comment
       end
-      issue.journals.create!(notes: build_comment(issue))
     end
 
     private
 
-    def build_comment(issue)
+    def build_comment
       <<~END
-        Вакансия: #{api_data[:vacancy_city]}, #{api_data[:vacancy_link]}
-        ФИО: #{api_data[:applicant_last_name]} #{api_data[:applicant_first_name]} #{api_data[:applicant_middle_name]}
-        Город: #{api_data[:applicant_city]}
-        Дата рождения: #{api_data[:applicant_birth_date]}
-        Резюме: #{api_data[:resume_link]}
-        Фото: #{api_data[:applicant_photo]}
-        Зарплата: #{api_data[:salary] || 'не указана'}
-        Email: #{api_data[:applicant_email]}
+        *Вакансия:* #{api_data[:vacancy_link]} (#{api_data[:vacancy_city]})
+        *Имя:* #{api_data[:applicant_first_name]} #{api_data[:applicant_last_name]}
+        *Город:* #{api_data[:applicant_city]}
+        *Дата рождения:* #{api_data[:applicant_birth_date]&.to_date&.strftime('%d.%m.%Y')}
+        *Резюме:* #{api_data[:resume_link]}
+        *Фото:* #{api_data[:applicant_photo]}
+        *Зарплата:* #{api_data[:salary] || 'не указана'}
+        *E-mail:* #{api_data[:applicant_email]}
 
-        Описание: #{api_data[:description]}
+        *О себе:*
+        #{api_data[:description]}
 
-        Предыдущие места работы:
+        *Сопроводительное письмо:*
+        #{api_data[:cover_letter]}
+
+        *Предыдущие места работы:*
         #{previous_works(api_data[:experience])}
 
-        Предыдущие отклики:
-        #{previous_issues(issue)}
+        *Предыдущие отклики:*
+        #{previous_issues}
       END
     end
 
@@ -53,18 +57,17 @@ module Hh
     def previous_works(works)
       works.map do |work|
         <<~END
-          период: #{work['start']} - #{work['end'] || 'наст. время'} (#{exp_in_monthes(work['start'], work['end'])} мес.)
-          город: #{work['area']['name'] if work['area'].present?}
-          компания: #{work['company']}
-
-          опыт:
+          *Период:* #{work['start'].to_date.strftime('%d.%m.%Y')} - #{work['end']&.to_date&.strftime('%d.%m.%Y') || 'наст. время'} (#{exp_in_monthes(work['start'], work['end'])} мес.)
+          *Город:* #{work['area']['name'] if work['area'].present?}
+          *Компания:* #{work['company']}
+          *Опыт:*
           #{work['description']}
         END
       end.join("\n")
     end
 
-    def previous_issues(current_issue)
-      previous_issues_ids = Issue.where(resume_id: api_data[:resume_id]).pluck(:id) - [current_issue.id]
+    def previous_issues
+      previous_issues_ids = Issue.where(resume_id: api_data[:resume_id]).pluck(:id)
       previous_issues_ids.map do |issue_id|
         "##{issue_id}"
       end.join(' ')
