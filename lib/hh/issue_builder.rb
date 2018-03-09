91d34c41f8e718e784c846d3928f799ecb3a50f8
Vladislav Yashin <v.yashin.work@gmail.com>
2018-03-09 14:51:20 +0300

    Get rid of Redmine API usage

--- lib/hh/issue_builder.rb
+++ lib/hh/issue_builder.rb
@@ -8,7 +8,6 @@ module Hh
     ISSUE_STATUS_NAME = Setting.plugin_redmine_hire['issue_status']
     ISSUE_TRACKER_NAME = Setting.plugin_redmine_hire['issue_tracker']
     ISSUE_AUTHOR = Setting.plugin_redmine_hire['issue_author']
-    REDMINE_API_KEY = Setting.plugin_redmine_hire['redmine_api_key']
 
     attr_reader :api_data
 
@@ -20,74 +19,35 @@ module Hh
       return if Issue.where(vacancy_id: api_data[:vacancy_id], resume_id: api_data[:resume_id]).present?
       new_issue_status = IssueStatus.find_or_create_by!(name: ISSUE_STATUS_NAME)
       new_issue_author = User.find_by(id: ISSUE_AUTHOR) || User.find_by(status: User::STATUS_ANONYMOUS)
-      if helpdesk_present?
-        response = helpdesk_api_post
-        raise "Helpdesk API Error" unless response.code.start_with?('2')
-        new_issue_id = response.body.gsub(/[^\d]/, '')
-      else
-        response = redmine_api_post
-        raise "Redmine API Error" unless response.code.start_with?('2')
-        new_issue_id = JSON.parse(response.body)['issue']['id']
-      end
-      Issue.find(new_issue_id).update!(
+
+      issue = Issue.create!(
+        project_id: Project.find_or_create_by!(name: PROJECT_NAME).id,
+        subject: build_subject,
+        tracker_id: Tracker.find_or_create_by!(name: ISSUE_TRACKER_NAME).id,
+        description: build_comment,
         vacancy_id: api_data[:vacancy_id],
         resume_id: api_data[:resume_id],
         status_id: new_issue_status&.id,
         author_id: new_issue_author&.id,
         hh_response_id: api_data[:hh_response_id]
       )
+
+      if helpdesk_present?
+        HelpdeskTicket.create!(
+          issue: issue,
+          customer: Contact.create!(
+            email: api_data[:applicant_email],
+            first_name: api_data[:applicant_first_name],
+            last_name: api_data[:applicant_last_name]
+          )
+        )
+        response = helpdesk_api_post
+        raise "Helpdesk API Error" unless response.code.start_with?('2')
+        new_issue_id = response.body.gsub(/[^\d]/, '')
+      end
     end
 
     private
-
-    def helpdesk_api_post
-      uri = URI.parse "#{Setting['protocol']}://#{Setting['host_name']}/helpdesk/create_ticket.xml"
-      request = Net::HTTP::Post.new uri.path
-      request.content_type = 'application/xml'
-      request['X-Redmine-API-Key'] = REDMINE_API_KEY
-      request.body = build_xml
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true if Setting['protocol'] == "https"
-      response = http.request(request)
-    end
-
-    def build_xml
-      xm = Builder::XmlMarkup.new
-      xm.instruct!
-      xm.ticket {
-        xm.issue {
-          xm.project_id(Project.find_or_create_by!(name: PROJECT_NAME).id)
-          xm.subject(build_subject)
-          xm.tracker_id(Tracker.find_or_create_by!(name: ISSUE_TRACKER_NAME).id)
-          xm.description(build_comment)
-        }
-        xm.contact {
-          xm.email(api_data[:applicant_email])
-          xm.first_name(api_data[:applicant_first_name])
-          xm.last_name(api_data[:applicant_last_name])
-        }
-      }
-    end
-
-    def redmine_api_post
-      uri = URI.parse "#{Setting['protocol']}://#{Setting['host_name']}/issues.json"
-      request = Net::HTTP::Post.new uri.path
-      request.content_type = 'application/json'
-      request['X-Redmine-API-Key'] = REDMINE_API_KEY
-      request.body = {
-        issue: {
-          project_id: Project.find_or_create_by!(name: PROJECT_NAME).id,
-          subject: build_subject,
-          tracker_id: Tracker.find_or_create_by!(name: ISSUE_TRACKER_NAME).id,
-          description: build_comment,
-        }
-      }.to_json
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true if Setting['protocol'] == "https"
-      response = http.request(request)
-    end
 
     def build_comment
       previous_issues_ids = Issue.where(resume_id: api_data[:resume_id]).pluck(:id)
