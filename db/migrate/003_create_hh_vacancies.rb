cdb88fdf9cbbcc2a11adbfab46855a425db22134
Vladislav Yashin <v.yashin.work@gmail.com>
2018-03-12 10:27:00 +0300

    Use json instead of jsonb

--- db/migrate/003_create_hh_vacancies.rb
+++ db/migrate/003_create_hh_vacancies.rb
@@ -2,7 +2,7 @@ class CreateHhVacancies < ActiveRecord::Migration
   def change
     create_table :hh_vacancies do |t|
       t.bigint :hh_id, index: true
-      t.jsonb :info
+      t.json :info
       t.datetime :info_updated_at
     end
 
