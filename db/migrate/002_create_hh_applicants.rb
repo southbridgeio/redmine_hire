8a5ccc00468b94915a11140f1d84ab36ed6f12d4
Vladislav Yashin <v.yashin.work@gmail.com>
2018-07-25 15:23:40 +0300

    Revert "Use json instead of jsonb"
    This reverts commit cdb88fdf9cbbcc2a11adbfab46855a425db22134.

--- db/migrate/002_create_hh_applicants.rb
+++ db/migrate/002_create_hh_applicants.rb
@@ -2,7 +2,7 @@ class CreateHhApplicants < ActiveRecord::Migration
   def change
     create_table :hh_applicants do |t|
       t.string :hh_id, index: true
-      t.json :resume
+      t.jsonb :resume
       t.datetime :resume_updated_at
     end
 
